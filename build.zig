79145057e3bd9ddd6fd62ec8b1c70facc116e0ba
Lee Cannon <leecannon@leecannon.xyz>
2022-03-05 19:59:59 +0000

    build both 32 and 64 bit tests

--- build.zig
+++ build.zig
@@ -4,14 +4,20 @@ pub fn build(b: *std.build.Builder) void {
     b.prominent_compile_errors = true;
 
     const mode = b.standardReleaseOptions();
-    const target = std.zig.CrossTarget{ .cpu_arch = .riscv64 };
+    const target_32 = std.zig.CrossTarget{ .cpu_arch = .riscv32, .os_tag = .freestanding };
+    const target_64 = std.zig.CrossTarget{ .cpu_arch = .riscv64, .os_tag = .freestanding };
 
-    const tests = b.addTest("sbi.zig");
-    tests.setBuildMode(mode);
-    tests.setTarget(target);
+    const test_32 = b.addTestExe("test_32", "sbi.zig");
+    test_32.setBuildMode(mode);
+    test_32.setTarget(target_32);
+
+    const test_64 = b.addTestExe("test_64", "sbi.zig");
+    test_64.setBuildMode(mode);
+    test_64.setTarget(target_64);
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&tests.step);
+    test_step.dependOn(&test_64.step);
+    test_step.dependOn(&test_32.step);
 
     b.default_step = test_step;
 }
