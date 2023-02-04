32f91b46b60cef2a3d174e23d02d25ede797cbb4
Lee Cannon <leecannon@leecannon.xyz>
2023-02-04 18:51:43 +0000

    update for zig master

--- build.zig
+++ build.zig
@@ -1,19 +1,25 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.Build) void {
     b.prominent_compile_errors = true;
 
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
     const target_32 = std.zig.CrossTarget{ .cpu_arch = .riscv32, .os_tag = .freestanding };
     const target_64 = std.zig.CrossTarget{ .cpu_arch = .riscv64, .os_tag = .freestanding };
 
-    const test_32 = b.addTestExe("test_32", "sbi.zig");
-    test_32.setBuildMode(mode);
-    test_32.setTarget(target_32);
+    const test_32 = b.addTest(.{
+        .name = "test_32",
+        .root_source_file = .{ .path = "sbi.zig" },
+        .target = target_32,
+        .optimize = optimize,
+    });
 
-    const test_64 = b.addTestExe("test_64", "sbi.zig");
-    test_64.setBuildMode(mode);
-    test_64.setTarget(target_64);
+    const test_64 = b.addTest(.{
+        .name = "test_64",
+        .root_source_file = .{ .path = "sbi.zig" },
+        .target = target_64,
+        .optimize = optimize,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&test_64.step);
