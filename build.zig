b72c5fac810643bca9da3a4a3132226805f7711c
Lee Cannon <leecannon@leecannon.xyz>
2023-04-15 21:09:54 +0100

    update for zig master

--- build.zig
+++ build.zig
@@ -1,27 +1,34 @@
 const std = @import("std");
 
+// TODO: https://github.com/ziglang/zig/issues/15301
+const disable_risc32 = true;
+
 pub fn build(b: *std.Build) void {
+    const test_step = b.step("test", "Run library tests");
+
     const optimize = b.standardOptimizeOption(.{});
-    const target_32 = std.zig.CrossTarget{ .cpu_arch = .riscv32, .os_tag = .freestanding };
+
     const target_64 = std.zig.CrossTarget{ .cpu_arch = .riscv64, .os_tag = .freestanding };
 
-    const test_32 = b.addTest(.{
-        .name = "test_32",
-        .root_source_file = .{ .path = "sbi.zig" },
-        .target = target_32,
-        .optimize = optimize,
-    });
-
-    const test_64 = b.addTest(.{
+    const test_64 = b.addStaticLibrary(.{
         .name = "test_64",
         .root_source_file = .{ .path = "sbi.zig" },
         .target = target_64,
         .optimize = optimize,
     });
-
-    const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&test_64.step);
-    test_step.dependOn(&test_32.step);
+
+    if (!disable_risc32) {
+        const target_32 = std.zig.CrossTarget{ .cpu_arch = .riscv32, .os_tag = .freestanding };
+
+        const test_32 = b.addStaticLibrary(.{
+            .name = "test_32",
+            .root_source_file = .{ .path = "sbi.zig" },
+            .target = target_32,
+            .optimize = optimize,
+        });
+        test_step.dependOn(&test_32.step);
+    }
 
     b.default_step = test_step;
 }
