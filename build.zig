b71ff355807758db4832074791336e9f14f21354
Lee Cannon <leecannon@leecannon.xyz>
2024-01-08 18:07:00 +0000

    update to zig master

--- build.zig
+++ build.zig
@@ -1,34 +1,37 @@
 const std = @import("std");
 
-// TODO: https://github.com/ziglang/zig/issues/15301
-const disable_risc32 = true;
-
 pub fn build(b: *std.Build) void {
-    const test_step = b.step("test", "Run library tests");
-
+    const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const target_64 = std.zig.CrossTarget{ .cpu_arch = .riscv64, .os_tag = .freestanding };
+    const module = b.addModule("sbi", .{
+        .root_source_file = .{ .path = "sbi.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    _ = module;
 
-    const test_64 = b.addStaticLibrary(.{
+    buildTests(b, optimize);
+}
+
+fn buildTests(b: *std.Build, optimize: std.builtin.OptimizeMode) void {
+    const test_step = b.step("test", "Run library tests");
+
+    const target_64 = b.resolveTargetQuery(.{ .cpu_arch = .riscv64, .os_tag = .freestanding });
+    const build_test_64 = b.addStaticLibrary(.{
         .name = "test_64",
         .root_source_file = .{ .path = "sbi.zig" },
         .target = target_64,
         .optimize = optimize,
     });
-    test_step.dependOn(&test_64.step);
+    test_step.dependOn(&build_test_64.step);
 
-    if (!disable_risc32) {
-        const target_32 = std.zig.CrossTarget{ .cpu_arch = .riscv32, .os_tag = .freestanding };
-
-        const test_32 = b.addStaticLibrary(.{
-            .name = "test_32",
-            .root_source_file = .{ .path = "sbi.zig" },
-            .target = target_32,
-            .optimize = optimize,
-        });
-        test_step.dependOn(&test_32.step);
-    }
-
-    b.default_step = test_step;
+    const target_32 = b.resolveTargetQuery(.{ .cpu_arch = .riscv32, .os_tag = .freestanding });
+    const build_test_32 = b.addStaticLibrary(.{
+        .name = "test_32",
+        .root_source_file = .{ .path = "sbi.zig" },
+        .target = target_32,
+        .optimize = optimize,
+    });
+    test_step.dependOn(&build_test_32.step);
 }
--- zig.mod
+++
@@ -1,6 +0,0 @@
-id: l1hejzu2n2den1ssnis3panyouend1vo06qru2f71vhpf6xw
-name: sbi
-main: sbi.zig
-license: MIT
-description: Zig wrapper around the RISC-V SBI specification
-dependencies:
